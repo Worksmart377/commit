a47be23899e154910b66ebdeecf6bd2390d7d88c
Mrs_N <worksmart377@gmail.com>
2023-06-13 17:57:34 -0500

    trying to fix postgres error

--- commit/settings.py
+++ commit/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y7-p!h9w=8!^tfky2l$^mo4uteoy&2&yv5pxh!hs*!_d2zbzk!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['commit-planner.herokuapp.com', '127.0.0.1:8000', 'localhost', "127.0.0.1"]
 # 'commit-planner.herokuapp.com', '127.0.0.1:8000', 'localhost'
